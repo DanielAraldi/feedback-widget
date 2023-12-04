0932b101eed375175ce46892c1d2268b5247487b
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-12-04 15:56:25 -0300

    feat: added `toggleWidgetVisibility` function to toggle `Widget` component

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,19 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { useState } from 'react';
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState<boolean>(false);
+
+  function toggleWidgetVisibility(): void {
+    setIsWidgetOpen(!isWidgetOpen);
+  }
+
   return (
     <div className='absolute bottom-5 right-5'>
-      <button className='flex items-center bg-brand-500 rounded-full p-3 h-12 text-white group'>
+      <button
+        onClick={toggleWidgetVisibility}
+        className='flex items-center bg-brand-500 rounded-full p-3 h-12 text-white group'
+      >
         <ChatTeardropDots className='w-6 h-6' />
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
