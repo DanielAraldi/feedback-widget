ab382231a1ce9e58560be38f6078029d645eabf4
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-12-06 17:46:05 -0300

    style: added disabled style to `ScreenshotButton` component

--- web/src/components/ScreenshotButton/index.tsx
+++ web/src/components/ScreenshotButton/index.tsx
@@ -35,7 +35,7 @@ export function ScreenshotButton(props: ScreenshotButtonProps) {
   return (
     <button
       type='button'
-      className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:bg-zinc-700'
+      className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-zinc-800'
       onClick={handleTakeScreenshot}
       disabled={isTakingScreenshot}
     >
