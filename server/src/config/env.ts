3b30675370da336c4f73dec2e1bc3b7bc1c6956a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-12-07 16:11:47 -0300

    feat: added `nodemailer` to sent emails to receiver

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -3,5 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const envs = {
-  port: process.env.PORT || '3334',
+  port: Number.parseInt(process.env.PORT || '3334'),
+  hostMail: process.env.HOST_MAIL || 'localhost',
+  portMail: Number.parseInt(process.env.PORT_MAIL || '5432'),
+  userMail: process.env.USER_MAIL,
+  passwordMail: process.env.PASSWORD_MAIL,
+  toMail: process.env.TO_MAIL,
 };
--- server/src/routes/feedback-routes.ts
+++ server/src/routes/feedback-routes.ts
@@ -1,5 +1,18 @@
 import { Express } from 'express';
+import { createTransport } from 'nodemailer';
 import { prisma } from '../prisma';
+import { envs } from '../config';
+
+const { toMail, hostMail, passwordMail, portMail, userMail } = envs;
+
+const transport = createTransport({
+  host: hostMail,
+  port: portMail,
+  auth: {
+    user: userMail,
+    pass: passwordMail,
+  },
+});
 
 export function setupRoutes(app: Express): void {
   app.post('/feedbacks', async (request, response): Promise<void> => {
@@ -12,6 +25,18 @@ export function setupRoutes(app: Express): void {
         screenshot,
         createdAt: new Date(),
       },
+    });
+
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: `Daniel Sansão Araldi <${toMail}>`,
+      subject: 'Novo feedback',
+      html: [
+        '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        '</div>',
+      ].join('\n'),
     });
 
     response.status(201).json({ id });
