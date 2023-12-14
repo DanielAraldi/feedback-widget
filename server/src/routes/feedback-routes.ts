fe2eef727a5dc0b5cd7dd91c15c728e3a2e537bf
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-12-14 16:24:27 -0300

    fix: added screenshot image in html body email

--- server/src/routes/feedback-routes.ts
+++ server/src/routes/feedback-routes.ts
@@ -1,20 +1,7 @@
 import { Router } from 'express';
-import { createTransport } from 'nodemailer';
-import { envs } from '../config';
 import { SubmitFeedbackUseCase } from '../usecases';
 import { PrismaFeedbacksRepository } from '../repositories';
 import { NodemailerAdapter } from '../adapters';
-
-const { toMail, hostMail, passwordMail, portMail, userMail } = envs;
-
-const transport = createTransport({
-  host: hostMail,
-  port: portMail,
-  auth: {
-    user: userMail,
-    pass: passwordMail,
-  },
-});
 
 export function feedbackRoutes(router: Router): void {
   router.post('/feedbacks', async (request, response): Promise<void> => {
@@ -33,17 +20,5 @@ export function feedbackRoutes(router: Router): void {
     } catch (error) {
       response.status(500).json({ error });
     }
-
-    // await transport.sendMail({
-    //   from: 'Equipe Feedget <oi@feedget.com>',
-    //   to: `Daniel Sansão Araldi <${toMail}>`,
-    //   subject: 'Novo feedback',
-    //   html: [
-    //     '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
-    //     `<p>Tipo do feedback: ${type}</p>`,
-    //     `<p>Comentário: ${comment}</p>`,
-    //     '</div>',
-    //   ].join('\n'),
-    // });
   });
 }
--- server/src/usecases/cases/submit-feedback.ts
+++ server/src/usecases/cases/submit-feedback.ts
@@ -25,6 +25,7 @@ export class SubmitFeedbackUseCase implements SubmitFeedback {
         '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}" />` : null,
         '</div>',
       ].join('\n'),
     });
