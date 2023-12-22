dfd8f1affca27cc1ed1ce0b8542660ef387cb7dc
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-12-22 18:26:28 -0300

    fix: removed `createdAt` field in database that'll be unnecessary

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -3,11 +3,6 @@ import { FeedbackCreateData, FeedbacksRepository } from '../@types';
 
 export class PrismaFeedbacksRepository implements FeedbacksRepository {
   async create(data: FeedbackCreateData): Promise<void> {
-    await prisma.feedback.create({
-      data: {
-        ...data,
-        createdAt: new Date(),
-      },
-    });
+    await prisma.feedback.create({ data });
   }
 }
