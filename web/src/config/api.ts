9a109f59d126f9bc491af64f4a7de26b378ea604
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-12-23 10:01:09 -0300

    fix: removed environment responsible by port in web folder

--- web/src/config/api.ts
+++ web/src/config/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}:${import.meta.env.VITE_API_PORT}`,
+  baseURL: import.meta.env.VITE_API_URL,
 });
